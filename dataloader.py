17d60b18938b57c21c2b8aa7389a626012134ee2
HiddenBoiRightHere <a.denaughel@student.tue.nl>
2022-10-14 02:42:19 +0200

    Created functions to split bnk and wem file name data into separate categories.

--- dataloader.py
+++ dataloader.py
@@ -62,8 +62,8 @@ def wem_opener(path: str) -> List[Dict[str, str]]:
 def bnk_opener(path: str) -> List[Dict[str,str]]:
     """
     Retrieves all bnk file information if available, and places it in a list of dictionaries.
-    :param path:
-    :return: List of dictionaries containing ID, Name, Audio Source File, Wwise object path, and notes
+    :param path: File path string.
+    :return: List of dictionaries containing ID, Name, Audio Source File, Wwise object path, and notes.
     """
     with open(path) as file:
         #Creates empty list to store all information from file
@@ -125,29 +125,161 @@ def retrieve_categories(total_data: List[Dict[str,str]], total_bnk: List[Dict[st
     :param total_data: List of dictionaries containing all data.
     :return: lists?
     """
-    fighter_list = ["GlassJoe", "DiscoKid", "VonKaiser"]
-
-    crowd_list = []
-    sfx_list = []
-    opponent_list = []
-    other_list = []
+    #Creates empty dictionaries for the bnk and wem file names
+    bnk_dictionary = {}
+    wem_dictionary = {}
 
 
+    #if there is no data, exit the program
+    if (total_data == None and total_bnk == None):
+        exit("You don't have any wem or bnk data in here!")
+
+    elif(total_data != None and total_bnk == None):
+        wem_dictionary = wem_notes(total_data)
+        bnk_dictionary = None
+    elif (total_data == None and total_bnk != None):
+        wem_dictionary = None
+        bnk_dictionary = bnk_notes(total_bnk)
+    else:
+        wem_dictionary = wem_notes(total_data)
+        bnk_dictionary = bnk_notes(total_bnk)
+
+    return wem_dictionary, bnk_dictionary
+
+
+def doc_louis_notes(total_data: List[Dict[str,str]]) -> Dict[str, List[Dict[str,str]]]:
+    """
+    If the doc louis notes file is open, separate into per character advice, win, loss, and other.
+    :param total_data:
+    :return:
+    """
+
+    wem_dictionary = {}
+    # creates list of all opponents
+    category_list = ["Glass Joe", "Von Kaiser", "Disco Kid", "King Hippo", "Piston Honda", "Bear Hugger", "Great Tiger",
+                    "Don Flamenco", "Aran Ryan", "Soda Popinski", "Bald Bull", "Sandman", "Donkey Kong", "Win", "Loss", "Cornerman", "Other"]
+
+    # Creates their dictionary keys
+    for fighters in category_list:
+        wem_dictionary[fighters] = []
+
+    # for each file in the notes, match each file to their respective character and place in dictionary
     for elements in total_data:
         list_of_keys = list(elements.keys())
+        file_name = elements[list_of_keys[1]].lower()
 
-        file_name = elements[list_of_keys[1]]
-        internal_matches = ["CROWD", "Crowd"]
+        for fighters in category_list:
+            name = fighters.replace(" ", "")
 
-        if any(x in file_name for x in internal_matches):
-            crowd_list.append(elements)
+            if (name.lower() in file_name):
+                wem_dictionary[fighters].append(elements)
+                continue
+            else:
+                wem_dictionary["Other"].append(elements)
+                continue
+
+    print(wem_dictionary)
+    return wem_dictionary
+
+def wem_notes(total_data: List[Dict[str,str]]) -> Dict[str, List[Dict[str,str]]]:
+    """
+    If there are wem file notes, take total wem data and determine if it is from Doc Louis. If not, categorize into four categories.
+    :param total_data: All wem file data in list of dictionaries
+    :return: Dictionary per category containing list of more dictionaries with further information.
+    """
+
+
+    #creates dictionary template
+    wem_dictionary = {}
+
+    category_list = ["Crowd", "SFX", "Cornerman", "Other"]
+
+    #create template of all category names
+    for categories in category_list:
+        wem_dictionary[categories] = []
+
+    #for loop to go through total data
+    for elements in total_data:
+        list_of_keys = list(elements.keys())
+        file_name = elements[list_of_keys[1]].lower()
+
+        if ("doclouis" in file_name):
+            wem_dictionary = doc_louis_notes(total_data)
+            return wem_dictionary
         else:
             pass
 
-    print(total_bnk)
+        #categorizes each file name into respective list
+        for categories in category_list:
+            #removes any spaces in categories
+            name = categories.replace(" ", "")
+
+            if (name.lower() in file_name):
+                # if the category name is in the file name, put in dictionary with category
+                wem_dictionary[categories].append(elements)
+                continue
+
+            else:
+                #else place in other
+                wem_dictionary["Other"].append(elements)
+                continue
+
+    return wem_dictionary
 
 
+def bnk_notes(total_bnk):
+    """
+    If there are bnk file notes, take total bnk data and determine if it is from Doc Louis. If not, categorize into five categories.
+    :param total_bnk: All bnk file notes in one large list.
+    :return: Dictionary of categories containing lists of dictionaries with further information
+    """
+
+    bnk_dictionary = {}
+
+    category_list = ["SFX", "Reactions", "Punches", "Knockdown", "Other"]
+
+    for categories in category_list:
+        bnk_dictionary[categories] = []
+
+    #for loop to go through total data
+    for elements in total_bnk:
+        list_of_keys = list(elements.keys())
+        file_name = elements[list_of_keys[1]].lower()
+
+        if ("doclouis" in file_name):
+            bnk_dictionary = doc_louis_notes(total_bnk)
+            return bnk_dictionary
+        else:
+            pass
+
+         #categorizes each file name into respective list
+            for categories in category_list:
+
+                #removes any spaces in categories
+                name = categories.replace(" ", "")
+
+                if (name.lower() in file_name):
+                    # if the category name is in the file name, put in dictionary with category
+                    bnk_dictionary[categories].append(elements)
+                    continue
+
+                elif ("react" in file_name):
+                    bnk_dictionary["Reactions"].append(elements)
+                    continue
+
+                elif ("right" in file_name):
+                    bnk_dictionary["Punches"].append(elements)
+                    continue
+
+                elif ("left" in file_name):
+                    bnk_dictionary["Punches"].append(elements)
+                    continue
+
+                else:
+                    #else place in other
+                    bnk_dictionary["Other"].append(elements)
+                    continue
 
 
-
+    return bnk_dictionary
 
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from dataloader import *
 
+#flip = input("Please put a number 0-1 for different files. 0 is Glass Joe, 1 is Doc Louis: ")
+#flip = int(flip)
+
 flip = 0
 
 if flip == 0:
-    total_data = wem_opener("example_data/Glass_Joe.txt")
-    total_bnk = bnk_opener("example_data/Glass_Joe.txt")
+    total_data = wem_opener("example_data/SuperMachoMan.txt")
+    total_bnk = bnk_opener("example_data/SuperMachoMan.txt")
 else:
     total_data = wem_opener("example_data/DocLewis.txt")
     total_bnk = bnk_opener("example_data/DocLewis.txt")
